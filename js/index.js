82545433aa5f0eef4041307cb86b6335d10eb376
Justicefriday917 <justicefriday917@gmail.com>
2023-01-12 13:13:00 +0100

    create cart

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 import { products } from './data.js'
 
 const prods = document.getElementById('products')
@@ -16,26 +15,20 @@ modal.onclick = () => {
 
 } 
 
-const test = val => {
-  debugger
-  alert(val)
-}
+
 
 
 const rendermodal = () =>{
-  // const prod = data.map( item =>{
-  //   return item
-  // })
-  let mode= ''
- let  modal1 =`<img src="img/${products.img}" alt="app product">
+ let  modal1 =`<img src"" alt="app product">
     <div class="product-detail-modal">
-    <p>${products.name}</p>
-       <p>you just purcharsed it for ${products.price}</p>
+    <p>jikk</p>
+       <p>you just purcharsed it for </p>
        <button  class="success" id="btn1">succefully Added to your Cart</button>
     </div>`
-    mode +=modal1
-    modal.innerHTML = mode
+    modal.innerHTML=modal1
   }
+
+
 
 const renderProducts = () => {
   let div = ''
@@ -59,11 +52,71 @@ const renderProducts = () => {
   
 }
 
-window.addEventListener(`load`, (event) => {
 
-  renderProducts()
-  rendermodal()
+
+var removeButtonCart = document.getElementsByClassName("btn-remove")
+for (var i = 0; i < removeButtonCart.length; i++){
+  var button = removeButtonCart[i]
+  button.addEventListener("click", (e) =>{
+    var buttonclicked = e.target
+    buttonclicked.parentElement.parentElement.remove()
+    updateCartToal()
+  })}
+
+
+var QtyIput = document.getElementsByClassName("qtyInput")
+  for (var i = 0; i < QtyIput.length; i++){
+    const input = QtyIput[i]
+    input.addEventListener('change',(e) =>{
+      var click = e.target
+    if(isNaN(input.value) || input.value <= 0){
+      input.value = 1}
+    updateCartToal()
+  })}
+
+
+  function updateCartToal(){
+    const cartContainer = document.getElementsByClassName("cart")
+    let total = 0
+    for (var i = 0; i < cartContainer.length; i++){
+      const cartRow = cartContainer[i]
+      const priceElement = cartRow.getElementsByClassName("price")[0].innerText
+      const realPrice = parseFloat(priceElement.replace('$',''))
+      const Qty =cartRow.getElementsByClassName("qtyInput")[0]
+      const realQty = Qty.value
+      total +=(realPrice*realQty)
+  }
+    document.getElementsByClassName('total')[0].innerText= '$' + total
+  }
+
   
 
-})
+  // var addButtonCart = document.getElementsByClassName("cartitems")
+  // for (var i = 0; i < addButtonCart.length; i++){
+  //   var button = addButtonCart[i]
+  //   button.addEventListener("click", (e) =>{
+  //     var buttonclicked = e.target
+  //     const htmlCode = `
+  //       <div class="cart" id="cart">
+  //         <div class="cart-img">
+  //           <h4>gucci bag</h4>
+  //           <img src="img/logo.png" alt="my logo" class="cartImg">
+  //         </div>
+  //         <div>
+  //           <h4 class="price" id="price">$858</h4>
+  //         </div>
+  //         <div>
+  //           <input  class="qtyInput" type="number" value="1" style="width: 30px;">
+  //           <button class="btn-remove" id="btn-remove">Remove</button>
+  //         </div>
+  //       </div>`
+  //     buttonclicked.parentElement.append(htmlCode)
+  //      updateCartToal()
+  // })}
 
+
+window.addEventListener(`load`, (event) => {
+  renderProducts()
+  rendermodal() 
+  updateCartToal()
+})
